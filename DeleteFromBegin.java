70e78b2c9a772d08b1c21eaa5c92c4f59d6adabe
Niharika Malik <73269292+Niharikamalik@users.noreply.github.com>
2022-09-07 22:02:03 +0530

    Create DeleteFromBegin.java

---
+++ DeleteFromBegin.java
@@ -0,0 +1,37 @@
+class Main
+{
+	public static void main(String[] args) {
+	    Node head = new Node(10);
+	    head.next = new Node(20);
+	    head.next.next =new Node(30);
+	    head.next.next.next = new Node(40);
+	    head.next.next.next.next= new Node(50);
+	    printlist(head);
+	    head = deletebegin(head) ;
+	    System.out.println("After insertion : ");
+	    printlist(head);
+	}
+	static Node deletebegin(Node head){
+	    if(head==null)return null;
+	    else {
+	        return head.next;
+	    }
+        
+     }
+	static void printlist(Node head){
+	    Node cur = head;
+	    while (cur != null){
+	        System.out.print(cur.data+" ");
+	        cur = cur.next;
+	    }
+	    System.out.println();
+	}
+}
+class Node{
+        int data;
+        Node next;
+        Node(int x){
+            data = x;
+            next= null;
+        }
+    }
